1026774949e9a34c38cb959b46c536b6d1461f9a
jamesnottidge <freshice57@gmail.com>
2024-04-24 22:51:45 +0100

    toolset

--- src/components/layout/Toolset/Tool.tsx
+++ src/components/layout/Toolset/Tool.tsx
@@ -5,31 +5,31 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import LinkIcon from "@/components/ui/LinkIcon";
 
 
-export const Tool = ({  }: any) => {
- return (
-   <TooltipProvider>
-     <Tooltip>
-       <TooltipTrigger asChild>
-         <button>Hover</button>
-       </TooltipTrigger>
-       <TooltipContent
-       side="right"
-       >
-         <TextBubble>Add to library</TextBubble>
-       </TooltipContent>
-     </Tooltip>
-   </TooltipProvider>
- );
-};
 
-
-const TextBubble = ({ children }) => {
+export const Tool = ({
+    icon, 
+    text,
+}: any) => {
   return (
-    <div className="relative inline-block px-4 py-2 bg-gray-200 text-gray-800 rounded-lg">
-      <div className="absolute top-0 right-0 w-6 h-6 transform translate-x-1/2 -translate-y-1/2 bg-gray-200 rotate-45"></div>
-      {children}
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <button>
+            {icon}
+          </button>
+        </TooltipTrigger>
+        <TooltipContent
+          side="right"
+          className="bg-black text-white px-4 py-3 rounded-lg relative"
+        >
+          <p>{text}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
+
+
--- src/components/layout/Toolset/Toolset.tsx
+++ src/components/layout/Toolset/Toolset.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { Tool } from "./Tool";
+import LinkIcon from "@/components/ui/LinkIcon";
+import InvoiceIcon from "@/components/ui/InvoiceIcon";
+import MediaIcon from "@/components/ui/MediaIcon";
+import StoreIcon from "@/components/ui/StoreIcon";
 
 export const Toolset = () => {
   return (
-    <div className="fixed left-4 top-1/3 flex flex-col justify-between h-[20vh]">
-      <Tool />
-      <Tool />
-      <Tool />
-      <Tool />
-      <Tool />
+    <div className="fixed left-4 top-1/3 flex flex-col justify-between h-[20vh] border border-gray-200 py-4 px-2 rounded-full">
+      <Tool 
+        icon={<LinkIcon />}
+        text="Link In Bio"
+      />
+      <Tool 
+      icon = {<StoreIcon />}
+      text = "Store"
+      />
+      <Tool 
+      icon = {<MediaIcon />}
+        text = "Media Kit"
+      />
+      <Tool 
+      icon = {<InvoiceIcon />}
+        text = "Invoicing"
+      />
+    
     </div>
   );
 };
